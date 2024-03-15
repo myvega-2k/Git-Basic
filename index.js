82cebefa4e618593ca6ba17e4b5f50b8e26d87ef
myvega-2k <vega2k_@daum.net>
2024-03-15 15:49:59 +0900

    login passwd arg add

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 /*
     index.html 에서 참조하는 스크립트
 */
-function login() {
-    console.log("로그인")
+function login(password) {
+    if(password){
+        console.log("로그인")
+    }else{
+        console.log("password를 입력하세요")
+    }
 }
 
 function logout() {
