2e82d9bcf8b89c9c26b728f1f4fef36f5a180ef9
myvega-2k <vega2k_@daum.net>
2024-03-15 15:47:17 +0900

    login username arg add

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 /*
     index.html 에서 참조하는 스크립트
 */
-function login() {
-    console.log("로그인")
+function login(username) {
+    if(username){
+        console.log("로그인")
+    }else {
+        console.log('username을 입력하세요!!')
+    }
 }
 
 function logout() {
